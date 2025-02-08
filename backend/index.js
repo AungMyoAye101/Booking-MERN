7ea12da69d7a4c3cfe060365a934d8ad00e5e828
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-02-08 19:28:12 +0630

    Set up Express server with basic route and update package.json for module support

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,12 @@
+import express from "express";
 
+const PORT = 8080;
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello Elizbeth Olsen");
+});
+
+app.listen(PORT, () => {
+  console.log("server is listening on port " + PORT);
+});
