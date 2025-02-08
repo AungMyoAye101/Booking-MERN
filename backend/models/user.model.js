c74ddb8551dcb6e253305b8248f9896d57c04902
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-02-08 22:21:14 +0630

    Add user model schema

---
+++ backend/models/user.model.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+    },
+    city: {
+      type: String,
+    },
+    country: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.models("User") || mongoose.model("User", UserSchema);
+module.exports = User;
