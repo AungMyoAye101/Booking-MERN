21dab865964f9a76608543e27aafe3c80ad8509b
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-02-08 19:38:43 +0630

    Configure backend environment and update server to use dotenv for port management

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,15 @@
-import express from "express";
-
-const PORT = 8080;
+const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const port = process.env.PORT || 8080;
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("hello Elizbeth Olsen");
 });
-
-app.listen(PORT, () => {
-  console.log("server is listening on port " + PORT);
+app.get("/name", (req, res) => {
+  res.send(`<h1>Your name is Elizbeth </h1>`);
+});
+app.listen(port, () => {
+  console.log("server is listening on port " + port);
 });
