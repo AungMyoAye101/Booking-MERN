67e4b070e9d7a0b0ba203ebc13b1b4091bac96cf
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-02-09 19:42:53 +0630

    add hotel and room models with their respective schemas

---
+++ backend/models/hotel.model.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const hotelSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    photos: {
+      type: [String],
+    },
+    rating: {
+      type: Number,
+      min: 0,
+      max: 5,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    rooms: {
+      type: [String],
+    },
+
+    cheapestPrice: {
+      type: Number,
+      requried: true,
+    },
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+    distance: {
+      type: [String],
+    },
+
+    city: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Hotel = mongoose.models("Hotel") || mongoose.model("Hotel", hotelSchema);
+module.exports = Hotel;
--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -16,11 +16,6 @@ const UserSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
     image: {
       type: String,
     },
