494a01eb20b40a69509f9034d1ea34da583ec403
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-02-10 20:15:36 +0630

    Implement CRUD operations for users in controller and update routes

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -1,5 +1,41 @@
-const User = requrie("../models/user.model");
+const User = require("../models/user.model");
 
+//create user
+const createUser = async (req, res) => {
+  const newUser = new User(req.body);
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+//update User
+const updateUser = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(201).json(updatedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
+//delete user
+const deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User is successfully deleted.");
+  } catch (error) {
+    res.status(500).jsq(error);
+  }
+};
+
+//get all users
 const getAllUsers = async (req, res, next) => {
   try {
     const allUsers = await User.find();
@@ -8,6 +44,20 @@ const getAllUsers = async (req, res, next) => {
     next(error);
   }
 };
+//get user by id
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
+
 module.exports = {
+  createUser,
+  updateUser,
+  deleteUser,
   getAllUsers,
+  getUserById,
 };
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const {
+  getAllUsers,
+  createUser,
+  updateUser,
+  deleteUser,
+  getUserById,
+} = require("../controller/user");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("This is user route");
-});
+router.get("/", getAllUsers);
+router.post("/", createUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+router.get("/:id", getUserById);
 
 module.exports = router;
