9652d8a8feeb5c8cdad4ddfe75ad39b9e75de6a8
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-02-14 19:35:55 +0630

    Update Home page styling with background image, enhance layout, and add sign-up prompt

--- frontend/booking/src/pages/home/home.tsx
+++ frontend/booking/src/pages/home/home.tsx
@@ -2,7 +2,14 @@ import "./home.css";
 const Home = () => {
   return (
     <section className="home-container">
-      <div>Hello</div>
+      <div className="content_container">
+        <h1>A lifetime of discounts? It's Genius.</h1>
+        <p>
+          Get rewarded for your travels – unlock instant savings of 10% or more
+          with a free Lamabooking account
+        </p>
+        <button>Sign Up</button>
+      </div>
     </section>
   );
 };
