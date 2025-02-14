e6b05b3b27b347b0953f7ac796f5abbb67547896
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-02-14 19:06:37 +0630

    Enhance NavBar with login and signup links, update styles for improved UI, and adjust global styles for consistency

--- frontend/booking/src/components/NavBar/NavBar.tsx
+++ frontend/booking/src/components/NavBar/NavBar.tsx
@@ -6,11 +6,15 @@ const NavBar = () => {
   return (
     <nav className="nav_container">
       <Link to="/">
-        <h1>Booking</h1>
+        <h1 className="logo_title">Booking</h1>
       </Link>
-      <div className="flex-center">
-        <Link to={"/hotel"}>Hotel</Link>
-        <Link to={"/list"}>List</Link>
+      <div className="link_container">
+        <Link to={"/login"} className="link_btn">
+          Login
+        </Link>
+        <Link to={"/register"} className="link_btn">
+          Signup
+        </Link>
       </div>
     </nav>
   );
