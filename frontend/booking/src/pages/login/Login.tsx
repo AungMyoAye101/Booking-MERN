e9df68a5193ac1ce9e1f744b2a3297576aa327f9
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-02-17 21:25:40 +0630

    Add Zod validation to Login component; improve error handling and update styles

--- frontend/booking/src/pages/login/Login.tsx
+++ frontend/booking/src/pages/login/Login.tsx
@@ -1,12 +1,25 @@
 import { Link } from "react-router-dom";
 import "./login.css";
 import { useState } from "react";
+import { z } from "zod";
+
+const userValidation = z.object({
+  username: z
+    .string()
+    .min(3, { message: "username contain at least 6 character" })
+    .max(12, { message: "username doesn't exceed 12 character" }),
+  password: z
+    .string()
+    .min(6, { message: "password contain at least 6 character" })
+    .max(12, { message: "password doesn't exceed 12 character" }),
+});
 const Login = () => {
   const [user, setUser] = useState({
     username: "",
     password: "",
   });
   const [error, setError] = useState(false);
+  const [errorMessage, setErrorMEssage] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleChange = (e: { target: { name: string; value: string } }) => {
@@ -16,12 +29,14 @@ const Login = () => {
     e.preventDefault();
     setLoading(true);
     try {
-    } catch (error) {
+      userValidation.parse(user);
+      console.log(user);
+    } catch (error: any) {
       setError(true);
+      setErrorMEssage(error.errors[0].message);
       console.log(error);
     } finally {
       setLoading(false);
-      setError(false);
     }
     setUser({ username: "", password: "" });
   };
@@ -54,7 +69,7 @@ const Login = () => {
         >
           Submit
         </button>
-        {error && <p className="error-message">Username does't match</p>}
+        {error && <p className="error-message">{errorMessage}</p>}
       </form>
     </section>
   );
