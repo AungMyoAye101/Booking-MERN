480596f9be0b3405247ec700e91e15074365f29e
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-02-19 23:23:31 +0630

    Add dynamic hotel details page; update routing to include hotel ID and implement data fetching

--- frontend/booking/src/pages/hotel/hotel.tsx
+++ frontend/booking/src/pages/hotel/hotel.tsx
@@ -1,7 +1,20 @@
+import { useLocation } from "react-router-dom";
 import "./hotel.css";
+import useFetch from "../../hooks/usefetch";
 
 const Hotel = () => {
-  return <div>hotel</div>;
+  const location = useLocation();
+  const hotelId = location.pathname.split("/")[2];
+  const { data, loading, error } = useFetch(`api/hotel/${hotelId}`);
+  console.log(data);
+  return (
+    <section className="hotel-con">
+      <div>
+        <h1>{data.name}</h1>
+        <p>{data.description}</p>
+      </div>
+    </section>
+  );
 };
 
 export default Hotel;
