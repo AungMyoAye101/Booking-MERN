ce3347249430369cf5114aed001dc978455174fc
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-02-23 19:37:52 +0630

    Feat: Refactor NavBar and hero components for improved layout and styling;

--- frontend/booking/src/components/NavBar/NavBar.tsx
+++ frontend/booking/src/components/NavBar/NavBar.tsx
@@ -25,23 +25,25 @@ const NavBar = () => {
     }
   };
   return (
-    <nav className="nav_container">
-      <Link to="/">
-        <h1 className="logo_title">Booking</h1>
-      </Link>
-      <div className="link_container">
-        {user ? (
-          <button onClick={handleLogout}>Logout</button>
-        ) : (
-          <>
-            <Link to={"/login"} className="link_btn">
-              Login
-            </Link>
-            <Link to={"/signup"} className="link_btn">
-              Signup
-            </Link>
-          </>
-        )}
+    <nav>
+      <div className="fluid-container nav-con">
+        <Link to="/">
+          <h1 className="logo_title">Booking</h1>
+        </Link>
+        <div className="link_container">
+          {user ? (
+            <button onClick={handleLogout}>Logout</button>
+          ) : (
+            <>
+              <Link to={"/login"} className="link_btn">
+                Login
+              </Link>
+              <Link to={"/signup"} className="link_btn">
+                Signup
+              </Link>
+            </>
+          )}
+        </div>
       </div>
     </nav>
   );
