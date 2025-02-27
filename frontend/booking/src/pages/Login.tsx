36a893eb1772dc36aedbfce7182fc8464c80ba5c
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-02-27 13:01:16 +0630

    Feat: enhance login page UI with improved styling and structure

--- frontend/booking/src/pages/Login.tsx
+++ frontend/booking/src/pages/Login.tsx
@@ -51,35 +51,55 @@ const Login = () => {
   };
 
   return (
-    <section className="container">
-      <form className="form-container">
-        <h1>Login </h1>
-        <input
-          value={user.email}
-          type="email"
-          name="email"
-          placeholder="email"
-          onChange={(e) => handleChange(e)}
-        />
-        <input
-          value={user.password}
-          type="password"
-          name="password"
-          placeholder="password"
-          onChange={(e) => handleChange(e)}
-        />
-        <div className="link-con">
-          <p>Don't have an account?</p>
-          <Link to={"/signup"}>Signup</Link>
+    <section className="flex justify-center items-center min-h-screen">
+      <form className="bg-white min-w-80 max-w-96 h-fit py-8 px-6 border  rounded-lg shadow-lg flex flex-col gap-4 ">
+        <h1 className="text-center font-bold font-roboto text-2xl">Login </h1>
+        <label htmlFor="email" className="flex flex-col gap-1">
+          <span className="font-roboto text-sm"> Email</span>
+
+          <input
+            value={user.email}
+            id="email"
+            type="email"
+            name="email"
+            placeholder="example@gmail.com"
+            onChange={(e) => handleChange(e)}
+            className="bg-neutral-100 rounded p-2 border"
+          />
+        </label>
+        <label htmlFor="email" className="flex flex-col gap-1">
+          <span className="font-roboto text-sm">Password</span>
+          <input
+            value={user.password}
+            type="password"
+            name="password"
+            placeholder="password"
+            onChange={(e) => handleChange(e)}
+            className="bg-neutral-100 rounded p-2 border"
+          />
+        </label>
+        <div className="flex justify-between items-center ">
+          <p className="text-xs font-roboto opacity-80 ">
+            Don't have an account?
+          </p>
+          <Link
+            to={"/signup"}
+            className="font-roboto font-semibold text-xs hover:text-blue-700"
+          >
+            Signup
+          </Link>
         </div>
         <button
           onClick={onSubmitHandle}
           disabled={loading}
           style={{ cursor: loading ? "not-allowed" : "pointer" }}
+          className="bg-blue-800  p-2 rounded-lg font-roboto  text-white hover:bg-blue-500"
         >
           Submit
         </button>
-        {error && <p className="error-message">{errorMessage}</p>}
+        {error && (
+          <p className="bg-rose-500 text-white rounded-lg ">{errorMessage}</p>
+        )}
       </form>
     </section>
   );
