a948d0fadc24aa91bcc0a2c0fc220b6256975f39
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-02-28 21:14:06 +0630

    Feat: add hotel creation validation and enhance CreateHotel form with new fields

--- frontend/booking/src/lib/formValidation.ts
+++ frontend/booking/src/lib/formValidation.ts
@@ -18,3 +18,32 @@ export const signUpUserValidation = z.object({
     .max(12, { message: "password doesn't exceed 12 character" }),
   email: z.string().email(),
 });
+export const crateHotelValidation = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Hotel name contain at least 6 character" })
+    .max(12, { message: "Hotel name doesn't exceed 12 character" }),
+  title: z
+    .string()
+    .min(6, { message: "hotel title contain at least 6 character" })
+    .max(24, { message: "hotel title doesn't exceed 12 character" }),
+  desecription: z
+    .string()
+    .min(6, { message: "Description contain at least 6 character" }),
+
+  address: z
+    .string()
+    .min(3, { message: "Address contain at least 6 character" }),
+
+  photo: z
+    .string()
+    .array()
+    .min(1, { message: "Photo contain at least 1 photo" }),
+  distance: z
+    .string()
+    .array()
+    .min(1, { message: "Photo contain at least 1 photo" }),
+  city: z.string().min(3, { message: "City contain at least 6 character" }),
+
+  cheapestPrice: z.number(),
+});
--- frontend/booking/src/pages/CreateHotel.tsx
+++ frontend/booking/src/pages/CreateHotel.tsx
@@ -1,24 +1,50 @@
+import { useState } from "react";
+import { FaX } from "react-icons/fa6";
+
 const CreateHotel = () => {
+  const [hotel, setHotel] = useState({
+    name: "",
+    title: "",
+    description: "",
+    photos: [],
+    address: "",
+    cheapestPrice: 0,
+    city: "",
+    distance: [],
+    featured: false,
+    type: "",
+  });
+  const [photoArray, setPhotoArray] = useState<string[]>([]);
+  const [url, setUrl] = useState("");
+
+  // const handleChange = (e) => {
+  //   setHotel((pre) => ({ ...pre, [e.target.name]: e.target.value }));
+  // };
+
+  const handleKeyDown = (e: any) => {
+    if (e.key === "Enter") {
+      setPhotoArray((pre) => [...pre, url]);
+      setUrl("");
+    }
+  };
+  console.log(photoArray);
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const formData = new FormData(e.currentTarget); // Get form data
+    // const formData = new FormData(e.currentTarget); // Get form data
 
-    // Access form values
-    const name = formData.get("name");
-    const title = formData.get("title");
-    const description = formData.get("description");
-    const photo = formData.get("photo");
-    const city = formData.get("city");
+    // // Access form values
+    // const name = formData.get("name");
+    // const title = formData.get("title");
+    // const description = formData.get("description");
+    // const photo = formData.get("photo");
+    // const city = formData.get("city");
 
-    console.log({ name, title, description, photo, city });
+    // console.log({ name, title, description, photo, city });
   };
 
   return (
     <section className="py-20 flex justify-center">
-      <form
-        onSubmit={handleSubmit} // Use onSubmit instead of action
-        className="grid grid-cols-2 w-full gap-4 bg-white rounded-lg p-4 border"
-      >
+      <form className="grid grid-cols-2 w-full gap-4 bg-white rounded-lg p-4 border">
         <label htmlFor="name" className="flex flex-col gap-1">
           <span className="font-roboto text-sm">Name</span>
           <input
@@ -51,14 +77,53 @@ const CreateHotel = () => {
             className="bg-neutral-100 rounded p-2 border"
           />
         </label>
-
-        <label htmlFor="photo" className="flex flex-col gap-1">
-          <span className="font-roboto text-sm">Photo</span>
+        <div>
+          <label htmlFor="photo" className="flex flex-col gap-1">
+            <span className="font-roboto text-sm">Photo</span>
+            <input
+              disabled={photoArray.length >= 5}
+              id="photo"
+              type="text"
+              name="photo"
+              value={url}
+              placeholder="photo URL"
+              className="bg-neutral-100 rounded p-2 border"
+              onChange={(e) => setUrl(e.target.value)}
+              onKeyDown={(e) => handleKeyDown(e)}
+            />
+          </label>
+          <div className="flex gap-2 items-center mt-2">
+            {photoArray.length > 0 &&
+              photoArray.map((n, i) => (
+                <div
+                  key={i}
+                  className="flex justify-between items-center border border-gray-200 rounded-md w-32 gap-2 px-1.5 py-1 bg-blue-50  hover:bg-rose-100"
+                >
+                  <span className="text-xs line-clamp-1">{n}</span>
+                  <FaX
+                    onClick={() =>
+                      setPhotoArray(() => photoArray.filter((_, y) => y !== i))
+                    }
+                    className=" text-xs cursor-pointer"
+                  />
+                  {/* <button
+                    onClick={() =>
+                      setPhotoArray(() => photoArray.filter((_, y) => y !== i))
+                    }
+                  >
+                    <FaX className="text-lg " />
+                  </button> */}
+                </div>
+              ))}
+          </div>
+        </div>
+        <label htmlFor="distance" className="flex flex-col gap-1">
+          <span className="font-roboto text-sm">distance</span>
           <input
-            id="photo"
+            id="distance"
             type="text"
-            name="photo"
-            placeholder="photo URL"
+            name="distance"
+            placeholder="distance"
             className="bg-neutral-100 rounded p-2 border"
           />
         </label>
@@ -73,8 +138,28 @@ const CreateHotel = () => {
             className="bg-neutral-100 rounded p-2 border"
           />
         </label>
+        <label htmlFor="address" className="flex flex-col gap-1">
+          <span className="font-roboto text-sm">address</span>
+          <input
+            id="address"
+            type="text"
+            name="address"
+            placeholder="address"
+            className="bg-neutral-100 rounded p-2 border"
+          />
+        </label>
+        <label htmlFor="price" className="flex flex-col gap-1">
+          <span className="font-roboto text-sm">price</span>
+          <input
+            id="price"
+            type="number"
+            name="price"
+            placeholder="0"
+            className="bg-neutral-100 rounded p-2 border"
+          />
+        </label>
 
-        <button type="submit" className="btn h-fit">
+        <button onClick={(e) => handleSubmit(e)} className="btn h-fit">
           Create Hotel
         </button>
       </form>
