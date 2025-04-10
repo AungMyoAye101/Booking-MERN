472ab83af664b18c1f3ab651721e8dce12e3ec1b
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-04-10 22:35:50 +0630

    FEAT: add SideBar component for filtering search results in Search page

---
+++ frontend/booking/src/components/SideBar.tsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const SideBar = () => {
+    return (
+        <section className='w-60 p-4 rounded-lg bg-white border shadow-md '>
+            <div className='flex flex-col gap-1'>
+                <div className='flex gap-2 items-center'>
+                    <input type="checkbox" />
+                    <label htmlFor="" className='font-roboto text-sm'>5 stars</label>
+                </div>
+                <div>
+                    <input type="checkbox" />
+                    <label htmlFor="">5 stars</label>
+                </div>
+                <div>
+                    <input type="checkbox" />
+                    <label htmlFor="">5 stars</label>
+                </div>
+                <div>
+                    <input type="checkbox" />
+                    <label htmlFor="">5 stars</label>
+                </div>
+                <div>
+                    <input type="checkbox" />
+                    <label htmlFor="">5 stars</label>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default SideBar
--- frontend/booking/src/pages/Search.tsx
+++ frontend/booking/src/pages/Search.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { useSearch } from "../context/SearchContext";
+import SideBar from "../components/SideBar";
 
 
 
@@ -8,36 +9,40 @@ const Search = () => {
   const { searchData, loading } = useSearch()
 
   return (
-    <section className=" py-20 bg-red-400  flex flex-col gap-4 max-w-6xl mx-auto">
-      {
-        loading ? <h1>Loading...</h1> : searchData?.map((item) => (
-          <div
-            key={item._id}
-            className="flex-1 flex gap-4 bg-white rounded-lg p-4 shadow-lg "
+    <section className=" py-20 flex gap-4   max-w-6xl mx-auto">
+      <SideBar />
+      <div className="flex flex-col gap-4">
+        {
+          loading ? <h1>Loading...</h1> : searchData?.map((item) => (
+            <div
+              key={item._id}
+              className="flex-1 flex gap-4 bg-white rounded-lg p-4 shadow-lg "
 
-          >
-            <Link
-              to={`/hotel/${item._id}`}
-              className="w-40 aspect-square rounded-lg overflow-hidden "
             >
-              <img src={item.photos[0]} alt="hotel image" className="w-full h-full object-cover" />
-            </Link>
-            <div className="flex flex-col gap-1 flex-1">
-              <div className="flex justify-between items-center">
+              <Link
+                to={`/hotel/${item._id}`}
+                className="w-40 aspect-square rounded-lg overflow-hidden "
+              >
+                <img src={item.photos[0]} alt="hotel image" className="w-full h-full object-cover" />
+              </Link>
+              <div className="flex flex-col gap-1 flex-1">
+                <div className="flex justify-between items-center">
 
-                <h1 className="text-2xl font-roboto font-semibold">{item.name}</h1>
-                <div className="py-1 px-2 bg-blue-600 text-white rounded-md inline-block">
-                  {item.rating}
+                  <h1 className="text-2xl font-roboto font-semibold">{item.name}</h1>
+                  <div className="py-1 px-2 bg-blue-600 text-white rounded-md inline-block">
+                    {item.rating}
+                  </div>
                 </div>
+                <h2 className="font-roboto text-sm opacity-85 ">{item.title} </h2>
+                <p className=" font-serif ">{item.description}</p>
+                <button className="btn self-end">Reserve</button>
               </div>
-              <h2 className="font-roboto text-sm opacity-85 ">{item.title} </h2>
-              <p className=" font-serif ">{item.description}</p>
-              <button className="btn self-end">Reserve</button>
             </div>
-          </div>
-        ))
+          ))
 
-      }
+        }
+      </div>
+
 
     </section>
   );
