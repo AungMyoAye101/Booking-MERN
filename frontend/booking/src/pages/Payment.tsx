0abda1ca3533333bf9395226f474d91f84d09606
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-05-29 22:44:24 +0630

    FEAT: Revamp Payment page layout; add booking confirmation details and user input fields for name and email

--- frontend/booking/src/pages/Payment.tsx
+++ frontend/booking/src/pages/Payment.tsx
@@ -4,13 +4,14 @@ import { showToast } from '../context/ToastProvider'
 
 const Payment = () => {
     const [searchParams] = useSearchParams()
-    const room = searchParams.get('room') || ""
+    const hotel = searchParams.get('hotel') || ""
+    const roomName = searchParams.get('roomName') || ""
+    const roomId = searchParams.get('roomId') || ""
     const user = searchParams.get('user') || ""
     const roomNumber = searchParams.get('roomNumber') || ""
     const checkIn = searchParams.get('checkIn') || ""
     const checkOut = searchParams.get('checkOut') || ""
-    const checkInDate = new Date(checkIn)
-    const checkOutDate = new Date(checkOut)
+
 
 
     const roomBooking = async (roomId: string, roomNumber: number, userId: string, checkIn: Date, checkOut: Date) => {
@@ -43,9 +44,58 @@ const Payment = () => {
     }
 
     return (
-        <section className='h-screen items-center justify-center bg-blue-900'>
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus, voluptatem molestias? Quos aliquam cupiditate qui, ipsa sapiente, molestiae soluta sint dolorem non impedit, quod nam unde! Cumque perspiciatis rerum voluptates?</h1>
-            <button className='bg-red-400' onClick={() => roomBooking(room, Number(roomNumber), user!, checkInDate, checkOutDate)} >Pay Now</button>
+        <section className=" py-20 flex gap-4   max-w-6xl mx-auto">
+            <main>
+
+            </main>
+            <div>
+                <div className='bg-white shadow border rounded w-96 p-4 flex flex-col gap-2'>
+                    <h1 className='font-roboto text-2xl font-semibold mb-2'>Comfrim Your Detail</h1>
+                    <div>
+                        <label htmlFor="name" className='font-medium '>Your Name</label>
+                        <input className='border bg-neutral-200 p-1.5  w-full rounded' type="text" name='name' value={"your name"} placeholder='your name' />
+                    </div>
+                    <div>
+                        <label htmlFor="email" className='font-medium '>Your Email</label>
+                        <input className='border bg-neutral-200 p-1.5  w-full rounded' type="email" id='email' name='email' value={"your email"} placeholder='exampe@gmail.com' />
+                    </div>
+                    <div className='self-end'>
+                        <button className='btn '>Comfrim your booking</button>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div className='bg-white shadow border rounded w-80 p-4'>
+                    <h1 className='font-roboto text-2xl font-semibold mb-2'>Your Booking Summary</h1>
+                    <div className='flex justify-between items-center border-b py-1.5'>
+                        <h2 className='font-roboto font-semibold'>Hotel</h2>
+                        <p className='font-roboto'>Hotel </p>
+                    </div>
+                    <div className='flex justify-between items-center border-b py-1.5'>
+                        <h2 className='font-roboto font-semibold'>Room</h2>
+                        <p className='font-roboto'>Room</p>
+                    </div>
+                    <div className='flex justify-between items-center border-b py-1.5'>
+                        <h2 className='font-roboto font-semibold'>Room Numbers</h2>
+                        <p className='font-roboto'>12</p>
+                    </div>
+                    <div className='flex justify-between items-center border-b py-1.5'>
+                        <h2 className='font-roboto font-semibold'>CheckIn</h2>
+                        <p className='font-roboto'>{checkIn}</p>
+                    </div>
+                    <div className='flex justify-between items-center border-b py-1.5'>
+                        <h2 className='font-roboto font-semibold'>CheckOut</h2>
+                        <p className='font-roboto'>{checkOut}</p>
+                    </div>
+                    <div className='flex justify-between items-center  py-1.5'>
+                        <h2 className='font-roboto font-bold text-xl'>Total</h2>
+                        <p className='font-roboto font-semibold text-lg'>120 $</p>
+                    </div>
+
+                </div>
+            </div>
+            {/*          
+            <button className='bg-red-400' onClick={() => roomBooking(room, Number(roomNumber), user!, checkInDate, checkOutDate)} >Pay Now</button> */}
 
         </section>
     )
