de55a341109ec3250409204119dcec98e941cbca
Aung Myo Aye <aungmyoaye101@gmail.com>
2025-06-06 10:34:05 +0630

    FEAT: Enhance Dashboard component with total hotels display and improved layout

--- frontend/booking/src/components/Dashboard.tsx
+++ frontend/booking/src/components/Dashboard.tsx
@@ -1,9 +1,18 @@
-import React from "react";
+
 
 const Dashboard = () => {
   return (
-    <section className="flex flex-wrap gap-4">
-      <div className="bg-white w-96 h-60 rounded-lg shadow-lg"> hello</div>
+    <section className="flex flex-wrap gap-4 font-roboto ">
+      <div className="bg-white w-96 h-60 rounded-lg shadow-lg p-4 relative">
+        <h2 className="font-semibold text-lg absolute left-4 top-4  ">Total Hotels </h2>
+
+        <div className="flex h-full items-center justify-center ">
+
+          <h1 className="font-bold text-2xl   ">1000</h1>
+        </div>
+        <h2 className="font-semibold text-lg  absolute bottom-4 right-4"> Hotels </h2>
+      </div>
+
       <div className="bg-white w-96 h-60 rounded-lg shadow-lg"> hello</div>
       <div className="bg-white w-96 h-60 rounded-lg shadow-lg"> hello</div>
       <div className="bg-white w-96 h-60 rounded-lg shadow-lg"> hello</div>
